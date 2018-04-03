40fc2685c19b4cb0f77216d18f11332699a4d6b5
praseetha <praseethamdas198@gmail.com>
2018-04-03 15:04:32 +0530

    class method & objects

--- Eclient.java
+++ Eclient.java
@@ -1,2 +1,13 @@
 import java.net.*;  
 import java.io.*; 
+class Eclient
+{ 
+    public static void main(String args[])throws Exception
+    {    
+    Socket s=new Socket("localhost",3333);  
+    DataInputStream din=new DataInputStream(s.getInputStream());  
+    DataOutputStream dout=new DataOutputStream(s.getOutputStream());
+    BufferedReader br=new BufferedReader(new InputStreamReader(System.in));  
+    String str="",str2="";  
+    }
+}
