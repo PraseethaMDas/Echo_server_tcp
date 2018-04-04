1f487d57e0e93401c6c54886d75bf748a1e434a4
praseetha <praseethamdas198@gmail.com>
2018-04-04 11:07:47 +0530

    completed

--- Eserver.java
+++ Eserver.java
@@ -4,7 +4,7 @@ class Eserver
 {
 	public static void main(String args[])throws Exception
 	{
-		ServerSocket ss=new ServerSocket(3333);  
+		ServerSocket ss=new ServerSocket(4444);  
     	Socket s=ss.accept(); 
     	DataInputStream din=new DataInputStream(s.getInputStream());  
     	DataOutputStream dout=new DataOutputStream(s.getOutputStream());  
